dc3ef5fec6f07566bbc3e67ef11c99cb7ccce21c
Kyuubi0 <uftsimge@gmail.com>
2023-03-20 01:09:04 +0300

    1.1
    player movement eklendi - rigidbody yüklendi - ground ve player mataryeli eklendi - renk kondu - movement scripti eklendi

---
+++ lubalubadubdub/Assets/Codes/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private bool X;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(X);
+        Debug.Log("uwu");
+        print("uwu2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Debug.Log("uwu3");
+		print("uwu4");
+	}
+}
