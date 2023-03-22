00dae10487983d5b0cb91bc108553f278a72d246
Kyuubi0 <uftsimge@gmail.com>
2023-03-22 06:23:54 +0300

    1.3
    Score - GameManager - LevelComplete scriptleri yazıldı. UI oluşturuldu.

--- lubalubadubdub/Assets/Codes/PlayerMovement.cs
+++ lubalubadubdub/Assets/Codes/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -17,9 +18,16 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
+
         if(Input.GetKey("a")) 
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
+
+        if(rb.position.y < -1f)
+        {
+			FindObjectOfType<GameManager>().EndGame();
+        }
     }
+    
 }
---
+++ lubalubadubdub/Assets/Codes/Score.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public TMP_Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
